9ab48e7c9af3bc3996dcfc73ebca49c39d3d55b7
Garpepi H. Aotearoa <garpepi.aotearoa@cimbniaga.co.id>
2022-08-15 09:08:24 +0700

    Initial Commit

---
+++ rate-aps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rate-aps
+//
+//  Created by Garpepi Aotearoa on 15/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
